88eddb7423348210ab4cf24a3891ce2f44aa23b1
shingo.irie <>
2023-05-02 16:51:57 +0900

    python追加

---
+++ test.py
@@ -0,0 +1,19 @@
+def fetch_github_repositories_all_pages(url, headers):
+    page = 1
+    per_page = 100
+    all_items = []
+
+    while True:
+        paginated_url = f"{url}?page={page}&per_page={per_page}"
+        response = requests.get(paginated_url, headers=headers)
+        if response.status_code != 200:
+            break
+
+        items = response.json()
+        if not items:
+            break
+
+        all_items.extend(items)
+        page += 1
+
+    return all_items
